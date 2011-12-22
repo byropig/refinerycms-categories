0c5a8d4ed3da1c7dc29da9fbeb73d53775c357ce
byron <byron@perfectcircle.co.za>
2011-12-22 13:59:08 +0200

    updated categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,14 +6,42 @@ module Admin
             :include => [:parent],
             :title_attribute => 'name', :xhr_paging => true
 
-    after_filter :validate_children
+    def create
+          # if the position field exists, set this object as last object, given the conditions of this class.
+          if Category.column_names.include?("position")
+            params[:category].merge!({
+              :position => ((Category.maximum(:position)||-1) + 1)
+            })
+          end
+
+          if (@category = Category.create(params[:category])).valid?
+            
+              flash[:notice] = t('refinery.crudify.created', :what => @category.name)
+        
+              if @category.parent.present?
+                if @category.parent.products.present?
+                  flash[:notice] += " Please be aware that the '#{@category.parent.name}' category has products attached to it. Please re-assign these."
+                end
+              end                         
+              
+              redirect_to admin_categories_path            
+            
+          else            
+              render :action => 'new'            
+          end
+    end
     
-    def validate_children
+    def update
       
-      if Category.last.parent.present?
-        if Category.last.parent.products.present?
-          flash.now[:notice] = "Please be aware that the '#{Category.last.parent.name}' category has products attached to it. Please re-assign these."
+      if @category.update_attributes(params[:category])
+        
+        if @category.parent.present?
+          if @category.parent.products.present?
+            flash[:notice] = "Please be aware that the '#{@category.parent.name}' category has products attached to it. Please re-assign these."
+          end
         end
+        
+        redirect_to admin_categories_path
       end
         
     end
